ea54829741b356c5992d122880f3023e460eb211
brennanfoo <brennan.foo@gmail.com>
2016-01-03 18:05:22 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password # https://www.railstutorial.org/book/modeling_users#sec-a_hashed_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true;
 end
